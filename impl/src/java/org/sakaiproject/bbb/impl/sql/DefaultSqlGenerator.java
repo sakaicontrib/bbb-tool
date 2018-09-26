dda1f8f58c0ec2ba29b6a8752c310d51d6be305f
jfederico <jesus@123it.ca>
2018-09-26 19:29:45 +0000

    Updated copyright

--- impl/src/java/org/sakaiproject/bbb/impl/sql/DefaultSqlGenerator.java
+++ impl/src/java/org/sakaiproject/bbb/impl/sql/DefaultSqlGenerator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2009 The Sakai Foundation
+ * Copyright (c) 2010 onwards - The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
